1c22e2263fd26f260d15026b58775cb50408b337
CarrC2021 <carrc21@up.edu>
2020-03-23 20:43:15 -0700

    checkpoint 3 completed, if you run the code you will see the errors identified and I commented why they are broken

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -14,8 +14,10 @@ public class TwoPointsTest {
         points.setPoint(0, 2,7);
         points.setPoint(1,-1029, 8754);
         points.getPoint(0);
-        assertEquals("values tested are points", new Point(2 , 7), points.getPoint(0));
-        assertEquals("values tested are points", new Point(-1029, 8754), points.getPoint(1));
+        Point p1 = new Point(2 , 7);
+        Point p2 = new Point(-1029, 8754);
+        assertEquals(2, points.getPoint(0).x);
+        assertEquals(-1029, points.getPoint(1).x);
     }
 
     @Test
@@ -23,9 +25,11 @@ public class TwoPointsTest {
         TwoPoints points = new TwoPoints();
         points.setPoint(0, 2,7);
         points.setPoint(1,-1029, 8754);
-        points.getPoint(0);
-        assertEquals("values tested are points", new Point(2 , 7), points.getPoint(0));
-        assertEquals("values tested are points", new Point(-1029, 8754), points.getPoint(1));
+        Point p1 = new Point(2 , 7);
+        Point p2 = new Point(-1029, 8754);
+        assertEquals(2, points.getPoint(0).x);
+        assertEquals(-1029, points.getPoint(1).x);
+        assertEquals(8754, points.getPoint(1).y);
     }
 
     @Test
@@ -34,6 +38,26 @@ public class TwoPointsTest {
         points.randomValue(0);
         points.randomValue(1);
 
+        assertEquals(points.getPoint(0).x, points.getPoint(0).y, 20);
+        assertEquals(points.getPoint(1).x, points.getPoint(1).y, 20);
+
+        points.randomValue(0);
+        points.randomValue(1);
+
+        assertEquals(points.getPoint(0).x, points.getPoint(0).y, 20);
+        assertEquals(points.getPoint(1).x, points.getPoint(1).y, 20);
+
+        points.randomValue(0);
+        points.randomValue(1);
+
+        assertEquals(points.getPoint(0).x, points.getPoint(0).y, 20);
+        assertEquals(points.getPoint(1).x, points.getPoint(1).y, 20);
+
+        points.randomValue(0);
+        points.randomValue(1);
+
+        assertEquals(points.getPoint(0).x, points.getPoint(0).y, 20);
+        assertEquals(points.getPoint(1).x, points.getPoint(1).y, 20);
     }
 
     @Test
@@ -41,12 +65,22 @@ public class TwoPointsTest {
         TwoPoints points = new TwoPoints();
         points.setOrigin(0);
         points.setOrigin(1);
-        assertEquals(new Point(0,0), points.getPoint(0));
-        assertEquals(new Point(0,0), points.getPoint(1));
+        assertEquals(0, points.getPoint(0).x);
+        assertEquals(0, points.getPoint(1).y);
     }
 
     @Test
     public void copy() {
+        TwoPoints points = new TwoPoints();
+        points.setOrigin(0);
+        points.setPoint(1, -13,4);
+        points.copy(1,0);
+        assertEquals(points.getPoint(1).x, points.getPoint(0).x);
+        assertEquals(points.getPoint(0).y, points.getPoint(1).y);
+        points.setPoint(1, -12, 4);
+        assertNotEquals(points.getPoint(1).y, points.getPoint(0).y);
+
+        //this fails because it sets one object to be equivalent to the other instead of just copying its variables over
     }
 
     @Test
@@ -74,16 +108,17 @@ public class TwoPointsTest {
         points.setPoint(0, 7, 3);
         points.setPoint(1, 234, -3);
         double result = 0.0;
-        int xDiff = 7 - 3;
-        int yDiff = 234 + 3;
-        result = xDiff / yDiff;
-        assertEquals(result, points.slope(), .001);
+        double xDiff = 7 - 3;
+        double yDiff = 234 + 3;
+        result = yDiff / xDiff;
+        assertEquals(result, points.slope(), .01);
         points.setPoint(0, 7, 3);
         points.setPoint(1, 456, -7);
-        result = 0.0;
         xDiff = 7 - 456;
         yDiff = 3 + 7;
-        result = xDiff / yDiff;
+        result = yDiff / xDiff;
         assertEquals(result, points.slope(), .001);
+
+        //this throws an error because the slope method is flawed and uses xDiff/yDiff instead of yDiff/xDiff
     }
 }
